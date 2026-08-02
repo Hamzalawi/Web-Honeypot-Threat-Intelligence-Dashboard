bd34957ce81be03ea719c5c75db55f32f2de1da0
Hamzalawi <hamza.denguirchaabani@supcom.tn>
2026-08-03 00:11:38 +0100

    doing the dashboard api; read about connection tied requests and coded it, added different routes corresponding to different functions

--- dashboard-api/app.py
+++ dashboard-api/app.py
@@ -1,21 +1,26 @@
-from flask import Flask
+from flask import Flask, g, jsonify
 import pymysql
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
 
-
-connection = pymysql.connect(host=os.environ.get("DB_HOST"),
+def get_db():
+    if 'db' not in g:
+        g.db= pymysql.connect(host=os.environ.get("DB_HOST"),
                              user=os.environ.get("DB_USER"),
                              password=os.environ.get("DB_PASSWORD"),
                              database=os.environ.get("DB_NAME"),
                             cursorclass=pymysql.cursors.DictCursor
     )
+    return g.db
 
 @app.route("/")
 def recent_attacks():
+    
+    connection = get_db
 
     with connection:
         with connection.cursor() as cursor: 
@@ -25,16 +30,30 @@ def recent_attacks():
             limit 10
         """
             cursor.execute(sql)
-            result = cursor.fetchone()
+            result = cursor.fetchall()
 
-    return result
+    return jsonify(result)
 
+@app.route("/creds")
 def most_used_creds():
-    pass 
 
+    connection = get_db
 
+    with connection:
+        with connection.cursor() as cursor: 
+            sql =   """
+            select username, password from logins 
+            group by username
+        """
+            cursor.execute(sql)
+            result = cursor.fetchall() #wrong query, have to rewrite it 
+
+    return jsonify(result)
+
+@app.route("/stats")
 def stats():
-    pass 
+ 
+    pass
 
     #this function returns some stats such as: bots/humans pourcentage, most used tools against me 
 
